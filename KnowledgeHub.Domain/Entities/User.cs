21070538244640f62800bc17202d273e8a1574e0
JudyAugustine <12unnichan34@gmail.com>
2026-06-23 06:10:55 +0200

    Add User domain entity

---
+++ KnowledgeHub.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeHub.Domain.Entities;
+
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime CreatedAtUtc { get; set; }
+    }
+
