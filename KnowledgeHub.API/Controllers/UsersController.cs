bc37e3915d70f11760066fbdd9849193016474b9
JudyAugustine <12unnichan34@gmail.com>
2026-06-23 17:35:09 +0200

    Add users endpoint with EF Core and SQLite

---
+++ KnowledgeHub.API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using KnowledgeHub.Domain.Entities;
+using KnowledgeHub.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowledgeHub.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly AppDbContext _dbContext;
+
+    public UsersController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await _dbContext.Users.ToListAsync();
+
+        return Ok(users);
+    }
+
+    
+ 
+}
